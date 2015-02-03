f862527b4698ed2c84dcd929ae194239eb1fcc52
yupeng <penyu@amazon.com>
2015-02-03 03:36:16 +0000

    test db insert

--- src/db_op.py
+++ src/db_op.py
@@ -1,11 +1,75 @@
 #!/usr/bin/env python
 
+import logging
+import os
+import yaml
+from pymongo import MongoClient
+
+logger = logging.getLogger(__name__)
+
+with open('%s/conf.yaml' % os.path.split(os.path.realpath(__file__))[0], 'r') as f:
+    conf = yaml.safe_load(f)
+
+mongodb_addr = conf['mongodb_addr']
+mongodb_port = conf['mongodb_port']
+db_name = conf['db_name']
+data_history_len = conf['data_history_len']
+
+data_collection_name = conf['data_collection_name']
+
+client = MongoClient(mongodb_addr, mongodb_port)
+db = client[db_name]
+data_collection = db['data_collection_name']
+
 class Account(object):
-    def __init___(self):
-        self.coount = 0
+    def __init___(self, item):
+        self.item = item
+        self.account_id = item['_id']
+        if 'data' in item:
+            self.data = item['data']
+        else:
+            logger.warning('account without data: %s' % self.account_id)
+            self.data = []
+        self.count = len(self.data)
+
+    def set_metadata(self, name, value):
+        metadata = {name, value}
+        self.item.update(metadata)
+        ret = data_collection.update({'_id': self.account_id}, metadata)
+        if ret['updatedExisting'] is not True:
+            logger.error('set metadata failed: %s %s %s' % (self.account_id,  name, value))
+
+    def get_metadata(self, name):
+        if name in self.item:
+            return item['name']
+        else:
+            return '0'
+
+    def get_data(self, index, name):
+        if index > self.count or index < 0:
+            raise Exception('out of range: %s %d %d %s' % (self.account_id, self.count, index, name))
+        real_index = self.count - index
+        data = self.data[real_index]
+        if name not in data:
+            raise Exception('no such value: %s %d %d %s' % (self.account_id, self.count, index, name))
+        return data[name]
+        
 
 def get_accounts():
-    return None
+    items = data_collection.find()
+    for item in items:
+        yield Account(item)
 
-def set_metadata_by_account():
-    return None
+def set_metadata_by_account(account_id, metadata_name, value):
+    primary = {'_id': account_id}
+    ret = data_collection.update(primary, {metadata_name: value})
+    if ret['updatedExisting'] is False:
+        logger.warning('set metadata failed: %s %s %s' % (account_id, metadata_name, value))
+
+def insert_data(account_id, date, data):
+    primary = {'_id': account_id}
+    data.update({'date': date})
+    ret = data_collection.update(primary, {'$push': {'data': {'$each':[data], '$slice':-data_history_len}}})
+    if ret['updatedExisting'] is False:
+        primary.update(data)
+        data_collection.insert(primary)
---
+++ tests/test_db_op.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import unittest
+import os
+import datetime
+import time
+import yaml
+from pymongo import MongoClient
+import db_op
+
+base_dir = os.path.split(os.path.realpath(__file__))[0]
+conf_path = '%s/../src/conf.yaml' % base_dir
+with open(conf_path) as f:
+    conf = yaml.safe_load(f)
+
+mongodb_addr = conf['mongodb_addr']
+mongodb_port = conf['mongodb_port']
+data_history_len = conf['data_history_len']
+db_name = 'test_lifecycle_db'
+data_collection_name = 'test_lifecycle_collection'
+client = MongoClient(mongodb_addr, mongodb_port)
+db = client[db_name]
+data_collection = db['data_collection_name']
+
+fake_accounts = [
+    {
+        'account_id': '111',
+        'date': '2013/12/25',
+        'data': {'ka': 'va1', 'kb': 'vb1'},
+    },
+    {
+        'account_id': '111',
+        'date': '2013/12/26',
+        'data': {'ka': 'va2', 'kb': 'vb2'},
+    },
+    {
+        'account_id': '111',
+        'date': '2013/12/27',
+        'data': {'ka': 'va3', 'kb': 'vb3'},
+    },
+    {
+        'account_id': '222',
+        'date': '2013/12/28',
+        'data': {'ka': 'va4', 'kb': 'vb4'},
+    },
+]
+
+class DbOpTest(unittest.TestCase):
+
+    def setUp(self):
+        data_collection.drop()
+        db_op.data_collection = data_collection
+        db_op.data_history_len = 2
+
+    def tearDown(self):
+        data_collection.drop()
+
+    def test_insert_data(self):
+        account0 = fake_accounts[0]
+        db_op.insert_data(account0['account_id'], account0['date'], account0['data'])
+        results = list(data_collection.find())
+        self.assertEqual(len(results), 1)
+        result = results[0]
+        self.assertEqual(result['_id'], account0['account_id'])
+        account1 = fake_accounts[1]
+        db_op.insert_data(account1['account_id'], account1['date'], account1['data'])
+        account2 = fake_accounts[2]
+        db_op.insert_data(account2['account_id'], account2['date'], account2['data'])
+        results = list(data_collection.find())
+        self.assertEqual(len(results), 1)
+        result = results[0]
+        self.assertEqual(result['_id'], account0['account_id'])
+        self.assertEqual(len(result['data']), 2)
+        self.assertEqual(result['data'][1]['date'], account2['date'])
+        self.assertEqual(result['data'][1]['ka'], account2['data']['ka'])
+        self.assertEqual(result['data'][1]['kb'], account2['data']['kb'])
+        self.assertEqual(result['data'][0]['date'], account1['date'])
+        self.assertEqual(result['data'][0]['ka'], account1['data']['ka'])
+        self.assertEqual(result['data'][0]['kb'], account1['data']['kb'])
